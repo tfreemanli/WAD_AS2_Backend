43f5a58bb1b6cd31931c007e1f78d7e0f90e249a
Freeman-HP <kingkongenzo@gmail.com>
2025-06-11 22:43:05 +1200

    rollback, /admin/ to unique url.

--- Backend/urls.py
+++ Backend/urls.py
@@ -22,7 +22,6 @@ from TWRL.views import logout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', admin.site.urls),
     path('api/', include('TWRL.urls')),
     path('api/auth/', obtain_auth_token, name="obtain_auth_token"),
     path('api/auth/logout/', logout, name="logout"),
