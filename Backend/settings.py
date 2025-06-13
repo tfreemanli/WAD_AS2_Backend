58ba2d240eee2508b74ecf44d55a4edf60329224
Freeman-HP <kingkongenzo@gmail.com>
2025-06-13 22:11:16 +1200

    nothing

--- Backend/settings.py
+++ Backend/settings.py
@@ -111,6 +111,8 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
+
+
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'neondb',
         'USER': 'neondb_owner',
